f890a7ae59ba1c9993fbdb9bc9059dce955f171d
lanangwisana <lanangwisana@gmail.com>
2026-02-04 18:25:24 +0700

    update constants: import defaultEventImage

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,12 @@
 // App Constants - Same APP_ID as Admin for shared data
 export const APP_ID = 'bumi-adipura-8ed0a';
 
+import defaultEventImage from '../assets/defualt-image-events.png';
+
 // Asset URLs
 export const LOGO_URL = "https://lh3.googleusercontent.com/d/1oPheVvQCJmnBBxqfBp1Ev9iHfebaOSvb";
 export const USER_PHOTO_URL = "https://images.unsplash.com/profile-1766810764004-0d86c0062c85image?ixlib=rb-4.0.3&auto=format&fit=crop&w=250&q=80";
+export const DEFAULT_EVENT_IMAGE = defaultEventImage;
 
 // Gemini API Configuration
 export const GEMINI_API_KEY = ""; // Add your API key here or use environment variable
