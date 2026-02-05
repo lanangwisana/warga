8623873ecc73685bbc18878e8c62a8d172d88483
lanangwisana <lanangwisana@gmail.com>
2026-02-05 11:02:38 +0700

    update index: rendering features NotificationModal

--- src/features/index.js
+++ src/features/index.js
@@ -1,6 +1,6 @@
 // Features exports
 export { LoginScreen } from './auth';
-export { DashboardHero, NewsCarousel, RecentReports } from './dashboard';
+export { DashboardHero, NewsCarousel, RecentReports, NotificationModal } from './dashboard';
 export { FinanceScreen, BillingWidget, TransparencyService, PaymentModal } from './finance';
 export { ProfileScreen, IdCardModal } from './profile';
 export { CompactIoT } from './iot';
